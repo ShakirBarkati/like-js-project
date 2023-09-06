8f10d04011bf32834e31ef1990783f4518201e70
Shakir Barkati <Shakirbarkati313@gmail.com>
2023-09-06 18:29:44 +0500

    Small Project

---
+++ script-first.js
@@ -0,0 +1,19 @@
+let istatus = document.querySelector("h5");
+let btn = document.querySelector("#add");
+let check = 0;
+
+btn.addEventListener("click", function () {
+    if (check == 0) {
+        istatus.innerHTML = 'Friend';
+        istatus.style.color = "green";
+        btn.innerHTML = "Remove Friend";
+        check = 1;
+    } else {
+        istatus.innerHTML = 'Remove';
+        istatus.style.color = "red"
+        btn.innerHTML = "Add Friend";
+        check = 0;
+    }
+});
+
+
